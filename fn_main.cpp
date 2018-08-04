35a92ee1469781f10e7b22d11700ea661d39db4e
Steve Mussmann <smussmann@gmail.com>
2018-08-04 13:51:56 -0600

    added new menu options

--- fn_main.cpp
+++ fn_main.cpp
@@ -9,7 +9,7 @@
 namespace opt = boost::program_options;
 
 //void parseComLine(int argc, char **argv, std::string &infile, std::string &popmap, std::string &outgroup, std::string &abcd, int &vsize);
-void parseComLine(int argc, char **argv, std::string &infile, std::string &popmap, std::string &abcd, int &vsize);
+void parseComLine(int argc, char **argv, std::string &infile, std::string &popmap, std::string &abcd, int &vsize, bool &two, bool &three, bool &four);
 
 int main(int argc, char** argv) {
 
@@ -18,35 +18,43 @@ int main(int argc, char** argv) {
 	//std::string outgroup;
 	std::string abcd;
 	int vsize;
+	bool two = false;
+	bool three = false;
+	bool four = false;
 
 	//parseComLine(argc,argv,infile,popmap,outgroup,abcd,vsize);
-	parseComLine(argc,argv,infile,popmap,abcd,vsize);
+	parseComLine(argc,argv,infile,popmap,abcd,vsize,two,three,four);
 
 	//std::cout << "Hello World!" << std::endl;
 
 	//fnFiles f(infile, popmap, outgroup, abcd, vsize);
-	fnFiles f(infile, popmap, abcd, vsize);
-	f.readfiles();
+	if(four == true)
+	{
+		fnFiles f(infile, popmap, abcd, vsize);
+		f.readfiles();
 
-	fnStats fs(f.getLength(), f.ABCDmap);
-	fs.findAncestral(f);
-	fs.calcAllFreqs(f, f.ABCDmap);
-	fs.calcFstats(f);
+		fnStats fs(f.getLength(), f.ABCDmap);
+		fs.findAncestral(f);
+		fs.calcAllFreqs(f, f.ABCDmap);
+		fs.calcFstats(f);
+	}
 
 	return 0;
 }
 
 //void parseComLine(int argc, char **argv, std::string &infile, std::string &popmap, std::string &outgroup, std::string &abcd, int &vsize)
-void parseComLine(int argc, char **argv, std::string &infile, std::string &popmap, std::string &abcd, int &vsize)
+void parseComLine(int argc, char **argv, std::string &infile, std::string &popmap, std::string &abcd, int &vsize, bool &two, bool &three, bool &four)
 {
 	opt::options_description desc("--- Option Descriptions ---");
 	desc.add_options()
 		("help,h", "Prints this help message.")
 		("infile,i", opt::value<std::string>(&infile)->required(), "Specifies the input alleles file name.")
 		("popmap,p", opt::value<std::string>(&popmap)->required(), "Specifies the population map.")
-		//("outgroup,o", opt::value<std::string>(&outgroup)->required(), "Specifies the outgroup taxon name.")
 		("samples,n", opt::value<int>(&vsize)->required(), "Specifies the number of samples in your input file.")
 		("abcd,a", opt::value<std::string>(&abcd)->required(), "Specify the file containing a map of your four populations.")
+		("two,2", opt::bool_switch(&two), "Use this option to run only the F2 statistic.")
+		("three,3", opt::bool_switch(&three), "Use this option to run the F2 and F3 statistics.")
+		("four,4", opt::bool_switch(&four), "Use this option to run the F2, F3, and F4 statistics.")
 	;
 
 	opt::variables_map vm;
@@ -73,6 +81,22 @@ void parseComLine(int argc, char **argv, std::string &infile, std::string &popma
 	{
 		std::cerr << std::endl << "ERROR: " << e.what() << std::endl << std::endl;
 		std::cout << desc << std::endl;
+		exit(EXIT_FAILURE);
+	}
+	if(two == false && three == false && four == false)
+	{
+		std::cerr << "You must select one option to calculate F statistics." << std::endl;
+		exit(EXIT_FAILURE);
+	}
+	if(two == true && three == true && four == true)
+	{
+		std::cerr << "You must select only one option to calculate F statistics." << std::endl;
+		exit(EXIT_FAILURE);
+	}
+
+	if((two == true && three == true) || (three == true && four == true) || (two == true && four == true))
+	{
+		std::cerr << "You must select only one option to calculate F statistics." << std::endl;
 		exit(EXIT_FAILURE);
 	}
 	
